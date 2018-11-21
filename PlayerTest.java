95a7403f29acbbdd93518e7692e3b3f884926cf6
Chloe <chloefoucher.cf@gmail.com>
2018-11-21 15:57:25 +0100

    "add test method : pet a pet"

--- PlayerTest.java
+++ PlayerTest.java
@@ -11,54 +11,69 @@ import org.junit.Test;
  */
 public class PlayerTest
 {
- 	private Player playerTest;
- 	private Chest chestTest;
- 	private Character characterTest;
-	
-	/**
- 	* Constructeur de la classe-test PlayerTest
- 	*/
-	public PlayerTest()
-	{
-	}
-	/**
- 	* Met en place les engagements.
- 	*
- 	* Méthode appelée avant chaque appel de méthode de test.
- 	*/
-	@Before
-	public void setUp() // throws java.lang.Exception
-	{
-    	playerTest = new Player("Joe",40);
-    	chestTest = new Chest("Chest1", 360, 1000, "I am the Chest1", 100, false);
-    	characterTest= new Character("Joe", 50, 40);
-    	// Initialisez ici vos engagements
-   	
-	}
+    private Player player;
+    private Chest chest;
+    private Character character;
+    private Pet petCat; 
+    
+    /**
+    * Constructeur de la classe-test PlayerTest
+    */
+    public PlayerTest()
+    {
+    }
+    
+    /**
+    * Met en place les engagements.
+    *
+    * Méthode appelée avant chaque appel de méthode de test.
+    */
+    @Before
+    public void setUp() // throws java.lang.Exception
+    {
+        player = new Player("Joe",40);
+        chest = new Chest("Chest1", 360, 1000, "I am the Chest1", 100, false);
+        character = new Character("Joe", 50, 40);
+        petCat = new Pet("Minou",1000, 50,"Cat"); 
+        // Initialisez ici vos engagements
+    
+    }
    /**
- 	* This test checks if the starting player has 100 points of life
-	*/
-	
-	@Test
-	public void CheckplayerHP()
-	{
-   	
-    	assertEquals(100,playerTest.getLP());
-   	
-	}
-	/**
- 	* This test checks if When the player dies, he has no money
- 	*/
- 	@Test
-	public void grabcontent()
-	{
-    	
- 	assertEquals(75,playerTest.getLP());
-    	
+    * This test checks if the starting player has 100 points of life
+    */
+    
+    @Test
+    public void CheckplayerHP()
+    {
+    
+        assertEquals(100,player.getLP());
+    
+    }
+    
+    /**
+    * This test checks if When the player dies, he has no money
+    */
+    @Test
+    public void grabcontent()
+    {
+        
+    assertEquals(75,player.getLP());
+        
 }
- @Test
-	public void CaracterisDead()
-	{
+    @Test
+    public void CaracterisDead()
+    {
 }
+
+    @Test
+    /**
+     * Test when a player pets a pet. He must earn money (1 piece) and the cat must lose 1 piece. 
+     */
+    public void testPetAPet() {
+        player.pet(petCat); 
+        assertEquals(1,player.getMoney());
+        assertEquals(999,petCat.getMoney());
+    }
+
 }
 
