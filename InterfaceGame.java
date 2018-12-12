a958818b606b3dd9f9657d1eefb2f856f903aa1a
Chloe <chloefoucher.cf@gmail.com>
2018-12-12 22:33:19 +0100

    comments

--- InterfaceGame.java
+++ InterfaceGame.java
@@ -4,13 +4,25 @@ import java.awt.event.*;
 import java.util.*; 
 import javax.swing.ImageIcon;
 /**
- * Write a description of class InterfaceGame here.
+ * The InterfaceGame class contains the Frame of the game.
+ * The frame is composed of 6 panels : 
+ *      Player : displays all elements about a player (class : InterfacePlayer)
+ *      Planet : displays all elements about a planet (class : InterfacePlanet
+ *      Description : this panel is empty. It is used to display information : 
+ *          - Interaction with a monster, a pet, a chest or a seller
+ *          - display the items of the player
+ *          This panel is update very often. 
+ *      Map : displays the map of the game 
+ *      Door : displays the possible exits of a room (class : InterfaceRoom)
+ *      Character : displays 4 buttons (class : InterfaceChar)
+ *  
  *
  * @author (Group 7)
  * @version (04/12/18)
  */
 public class InterfaceGame extends JFrame implements ActionListener
 {
+    //declaration of the different variables
     private JFrame gameFrame; 
     private JPanel panelDoor, panelMap, panelPlayer, panelPlanet, panelDescription, panelChar, panelFinal; 
     private JMenuBar menuBar;
@@ -26,13 +38,17 @@ public class InterfaceGame extends JFrame implements ActionListener
     private InterfaceDescription panelOfDescription; 
     private InterfaceMonster panelOfMonster;
     private Game gamebis; 
-    private int option; 
+    private int option; //possible option = YES or NO (YES = restart the game, NO = quit the game)
     /**
      * Constructor for objects of class InterfaceGame
+     * Creation of the 6 panels where it is added the 
+     * different panels which come from other classes 
      */
     public InterfaceGame(Game game)
     {
         gamebis = game; 
+        
+        //Instantation of objects from the different classes
         panelOfPlayer = new InterfacePlayer(game); 
         panelOfChar = new InterfaceChar(game);
         panelOfMap = new InterfaceMap(game);
@@ -40,11 +56,13 @@ public class InterfaceGame extends JFrame implements ActionListener
         panelOfRoom = new InterfaceRoom(game);
         panelOfPlanet = new InterfacePlanet(game);
         panelOfDescription = new InterfaceDescription(game);
-
+        
+        //frame creation
         gameFrame = new JFrame ("GAME");
         gameFrame.setDefaultCloseOperation(gameFrame.DISPOSE_ON_CLOSE);
         menuBar = new JMenuBar(); 
-
+        
+        //menu creation
         this.setJMenuBar(menuBar); 
         menuName = new JMenu("GAME"); 
         this.menuBar.add(menuName); 
@@ -54,31 +72,37 @@ public class InterfaceGame extends JFrame implements ActionListener
         menu2 = new JMenuItem ("Quit"); 
         this.menuName.add(menu2); 
         menu2.addActionListener(this);
-
+        
+        //panel door = displays exits
         panelDoor = new JPanel();
         panelDoor.setLayout(new GridLayout (1,1));
         panelDoor.add(panelOfRoom.getPanelFinal());
-
+        
+        //panel map = displays the map
         panelMap = new JPanel();
         panelMap.setLayout(new GridLayout (1,1));
         panelMap.add(panelOfMap.getPanelMap());
-
+        
+        //panel player = displays information about the player
         panelPlayer = new JPanel();
         panelPlayer.setLayout(new GridLayout (1,1));
         panelPlayer.add(panelOfPlayer.getPanelPlayer()); 
-
+        
+        //panel planet = displays information about the planet
         panelPlanet = new JPanel();
         panelPlanet.setLayout(new GridLayout (1,1));
         panelPlanet.add(panelOfPlanet.getPanelPlanet());
-
+        
+        //panel description = display many information (interaction with characters)
         panelDescription = new JPanel(); 
         panelDescription.setLayout(new GridLayout (1,1));
         panelDescription.add(panelOfDescription);
-
+        //panel character = 4 buttons to interact with the character
         panelChar = new JPanel(); 
         panelChar.setLayout(new GridLayout (1,1));
         panelChar.add(panelOfChar.getPanelChar());
-
+        
+        //final panel = contains all the previous panel
         panelFinal = new JPanel(); 
         panelFinal.setLayout(new GridLayout (2,3));
         panelFinal.add(panelDoor); 
@@ -87,7 +111,8 @@ public class InterfaceGame extends JFrame implements ActionListener
         panelFinal.add(panelChar); 
         panelFinal.add(panelDescription);
         panelFinal.add(panelPlanet);
-
+        
+        //add the final panel to the frame and displays the frame
         this.add(panelFinal); 
         this.pack();
         this.setVisible(true);
